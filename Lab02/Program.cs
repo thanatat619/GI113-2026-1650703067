5732ac876909cd0a704bdedad4d9e2457ae7ebb1
thanatat619 <thanatat.muan@bumail.net>
2026-08-24 16:28:01 +0700

    add lab2 header

---
+++ Lab02/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Student ID :Thanatat muangduang
+ * Name       :Lab2
+ * Section    :129B
+ * No.        :NA
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
