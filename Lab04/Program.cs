6343d7d26f77fb532e61da13c00e79e1a1a6257b
thanatat619 <thanatat.muan@bumail.net>
2026-09-13 14:22:45 +0700

    add-script

---
+++ Lab04/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Student ID :1650703067
+ * Name       :Thanatat Muangduang
+ * Section    :129B
+ * No.        :01
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lab04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+------------------------------+");
+            Console.WriteLine("|      NEW ADVENTURE           |");
+            Console.WriteLine("+------------------------------+");
+
+            Console.Write("Name your character: ");
+             String charName = Console.ReadLine();
+            Console.Write("Choose a class (1-3): ");
+            bool classOk = int.TryParse(Console.ReadLine(), out int classNum);
+            Console.Write("Starting luck (0.0-10.0): ");
+            bool luckOk = double.TryParse(Console.ReadLine(), out double luck);
+            Console.WriteLine($"{charName} the Class-{classNum} adventurer enters the dungeon. Luck: {luck}");
+
+            Console.Write("Choose difficulty (1-3): ");
+            int difficulty = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Difficulty set to {difficulty}");
+
+            Console.WriteLine("+------------------------------+");
+            Console.WriteLine("|           ITEM SHOP           |");
+            Console.WriteLine("+------------------------------+");
+            Console.Write("How many potions? ");
+            bool quantityOk = int.TryParse(Console.ReadLine(), out int quantity);
+            Console.WriteLine($"Valid input: {quantityOk}");
+            Console.WriteLine($"Quantity: {quantity}");
+
+            Console.WriteLine("+------------------------------+");
+            Console.WriteLine("|          SET VOLUME           |");
+            Console.WriteLine("+------------------------------+");
+            Console.Write("Set music volume (0.0-1.0): ");
+            bool volumeOk = double.TryParse(Console.ReadLine(), out double volume);
+            Console.WriteLine($"Valid input: {volumeOk}");
+            Console.WriteLine($"Volume: {volume}");
+
+            Console.WriteLine("+------------------------------+");
+            Console.WriteLine("|         NEW SAVE FILE         |");
+            Console.WriteLine("+------------------------------+");
+            Console.Write("Enter save name: ");
+            string saveName = Console.ReadLine();
+            Console.Write("Choose save slot (1-3): ");
+            bool slotOk = int.TryParse(Console.ReadLine(), out int slot);
+            Console.WriteLine($"Save name: {saveName}");
+            Console.WriteLine($"Valid input: {slotOk}");
+            Console.WriteLine($"Slot: {slot}");
+        }
+    }
+}
